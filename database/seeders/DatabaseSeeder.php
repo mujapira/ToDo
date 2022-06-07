b419bbf0270c23633a10ae32122380b69ce03925
Mujapira <malmasmello2@gmail.com>
2022-06-07 14:42:33 -0300

    Factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,18 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Task;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
+        $this->call([]);
+
+        User::factory(5)->create();
+        Category::factory(5)->create();
+        Task::factory(10)->create();
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
